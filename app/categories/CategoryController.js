97327d772c77bd813f05d00ca8e8cd8b874fba17
Jardel Bazi <jardelbazi@gmail.com>
2022-10-01 11:50:38 -0300

    fix: Listagem de Categorias

--- app/categories/CategoryController.js
+++ app/categories/CategoryController.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const slugify = require('slugify')
 
-router.get('/categories', (req, res) => {
-	res.send('Rota de Categorias')
+const Category = require('./Category')
+
+router.get('/admin/categories', (req, res) => {
+	Category.findAll().then(categories => {
+		res.render('admin/categories/index', { categories })
+	})
+})
+
+router.get('/admin/categories/new', (req, res) => res.render('admin/categories/new'))
+
+router.post('/admin/categories/save', (req, res) => {
+	let title = req.body.title
+
+	Category.create({ 
+		title,
+		slug: slugify(title, { lower: true })
+	}).then(() => res.redirect('/admin/categories'))
 })
 
 module.exports = router
